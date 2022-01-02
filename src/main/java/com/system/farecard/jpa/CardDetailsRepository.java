d0a661f717cc355bf097bdc8b55f422f435caad4
nagcse42 <nagcse42@gmail.com>
2022-01-02 09:38:08 +0400

    changes commited

--- src/main/java/com/system/farecard/jpa/CardDetailsRepository.java
+++ src/main/java/com/system/farecard/jpa/CardDetailsRepository.java
@@ -14,6 +14,6 @@ public interface CardDetailsRepository extends JpaRepository<CardDetails, Long> 
 
     @Transactional
     @Modifying
-    @Query("update CardDetails c set c.balance = latestBalance where c.cardNumber = cardNumber")
-    void updateCardBalance(@Param("cardNumber") String cardNumber, @Param("balance") double latestBalance);
+    @Query("update CardDetails c set c.balance=:balance where c.cardNumber=:cardNumber")
+    void updateCardBalance(@Param("cardNumber") String cardNumber, @Param("balance") double balance);
 }
